0ed1ab6035bff60e1c3e7cb4c95ec30763744620
Philipp <philxyphilxy@gmail.com>
2024-07-31 19:32:53 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+namespace constants {
+constexpr double pi(3.14159);
+constexpr double avogadro(6.0221413e23);
+constexpr double earthAcceleration(9.81);
+} // namespace constants
+
+
+
+
+int main() { constants::pi; }
