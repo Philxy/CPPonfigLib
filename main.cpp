44380d169559f61c218f12ff34170a3f26b227d4
Philipp <philxyphilxy@gmail.com>
2024-07-31 19:33:21 +0200

    added moon acc

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ namespace constants {
 constexpr double pi(3.14159);
 constexpr double avogadro(6.0221413e23);
 constexpr double earthAcceleration(9.81);
+constexpr double moonAcceleration(1.62);
 } // namespace constants
 
 
